0e4c5fd50fba876b1481b914179bf12c5534ad20
Alex Sohail <elementrat@gmail.com>
2020-12-12 17:38:04 -0800

    break cards into their own file

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # Store all secrets and configuration the bot relies on
-game_channel_id = 0
-bot_token = ""
+game_channel_id = 227935134836457472
+bot_token = "Nzg3NDUxNTY2NDQ0OTA0NDc5.X9VJaw.iZwMAXBDmFGcyp5AzUN_Katha7k"
